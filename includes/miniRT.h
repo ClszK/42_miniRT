772b234faf714ba8c25251ccaf4946f82b68af5f
Jehoon Lee <jeholee@cx2r1s1.42seoul.kr>
2024-04-23 04:22:40 +0900

    miniRT: main.c and miniRT.h clean

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: jeholee <jeholee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 07:49:09 by jeholee           #+#    #+#             */
-/*   Updated: 2024/04/19 10:24:32 by jeholee          ###   ########.fr       */
+/*   Updated: 2024/04/23 04:16:23 by jeholee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 # define MINIRT_H
 
 # include <unistd.h>
-# include <stdlib.h>
-# include <math.h>
 # include "vec3.h"
 # include "my_mlx.h"
 # include "mlx.h"
@@ -23,6 +21,22 @@
 # include "sphere.h"
 # include "camera.h"
 
+#define	UP          126
+#define	DOWN        125
+#define	LEFT        123
+#define	RIGHT       124
+
 typedef int t_bool;
+
+typedef struct s_minirt
+{
+	t_vars		vars;
+	t_data		image;
+	t_canvas	canvas;
+	t_camera	camera;
+	t_bool		rendering;
+}	t_minirt;
+
+t_minirt	mini_init(void);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,23 +6,24 @@
 /*   By: jeholee <jeholee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:54:01 by jeholee           #+#    #+#             */
-/*   Updated: 2024/04/21 09:54:35 by jeholee          ###   ########.fr       */
+/*   Updated: 2024/04/23 04:21:49 by jeholee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include <stdio.h>
 
-int main()
+t_minirt	mini_init(void)
 {
+	t_minirt	mini;
 	t_vars		vars;
 	t_data		image;
 	t_canvas	canvas;
 	t_camera	camera;
 
 	canvas = canvas_init(800, 640, 1.0);
-	camera = camera_init(canvas, point3_init(0, 0, 0), vec3_init(0.8, 0, 0.1)); // 방향 벡터는 0,0,0을 가져서는 안됨. 로직 추가필요.
-
+	camera = camera_init(canvas, point3_init(0, 0, 0), 
+											vec3_init(0, 0, 1));
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, canvas.image_width, 
 								canvas.image_height, "miniRT");
@@ -30,9 +31,36 @@ int main()
 								canvas.image_height); // 이미지 객체 생성
 	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, \
 								&image.line_length, &image.endian); // 이미지 주소 할당
-	render(canvas, camera, &image);
-	mlx_put_image_to_window(vars.mlx, vars.win, image.img, 0, 0);
-	mlx_key_hook(vars.win, key_hook, &vars); // esc key press event
-	mlx_loop(vars.mlx);
+	mini.rendering = FALSE;
+	mini.camera = camera;
+	mini.canvas = canvas;
+	mini.image = image;
+	mini.vars = vars;
+	return (mini);
+}
+
+int	test(t_minirt *mini)
+{
+	if (mini->rendering == TRUE){
+		printf("%f %f %f\n", mini->canvas.viewport_u.x, mini->canvas.viewport_u.y, mini->canvas.viewport_u.z);
+		render(mini->canvas, mini->camera, &mini->image);
+		mlx_put_image_to_window(mini->vars.mlx, mini->vars.win, \
+														mini->image.img, 0, 0);
+		mini->rendering = FALSE;
+	}
+	return (0);
+}
+
+int main()
+{
+	t_minirt	mini;
+
+	mini = mini_init();
+	render(mini.canvas, mini.camera, &mini.image);
+	mlx_put_image_to_window(mini.vars.mlx, mini.vars.win, mini.image.img, 0, 0);
+	// mlx_key_hook(vars.win, key_hook, &vars); // esc key press event
+	mlx_key_hook(mini.vars.win, key_hook, &mini); // esc key press event
+	mlx_loop_hook(mini.vars.mlx, test, &mini);
+	mlx_loop(mini.vars.mlx);
 	return (0);
 }
